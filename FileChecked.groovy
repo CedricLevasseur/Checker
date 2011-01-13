f55cac92cfa6637d2b7dad19f21b23f6419c47cd
VIDAL <vidal@new-host-2.home>
2011-01-13 21:12:24 +0100

    reformating 1 ;a fileChecked is a result of a Size checking

---
+++ FileChecked.groovy
@@ -0,0 +1,42 @@
+//package Checker;
+
+public class FileChecked {
+    
+	public static FILE_NOT_FOUND_OR_EMPTY = -1;
+    	public static FILE_SIZE_VALID = -3;
+    	public static FILE_SIZE_EXPECTED_INCORRECT = -2;
+
+	public static int ERRORPERCENTDEFAULT = 0.05;
+
+	private double errorPercent = 0;
+	private double error = 0;
+
+	private int errorStatus;
+
+
+
+	public double getErrorPercent() {
+		return errorPercent;
+	}
+
+	public double getError(){
+		return error;
+	}
+
+	public FileChecked(double fileLength, double fileLengthExpected , int errorPercent){
+		if (fileLength <= 0){
+		    errorStatus= FILE_NOT_FOUND_OR_EMPTY;
+		}
+		
+		if ( ((fileLengthExpected * (1-errorPercent)) >= fileLength  ) && ( fileLength <= (fileLengthExpected * (1+errorPercent))) ){
+		   //return FILE_SIZE_VALID;
+		   errorStatus=FILE_SIZE_VALID;		
+		}
+
+		error=Math.abs(fileLength-fileLengthExpected);
+		errorPercent=Math.round(error/fileLengthExpected * 100);
+	}	
+			
+
+
+}
