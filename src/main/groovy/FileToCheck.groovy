cd1cfaf301b2c11d4947a9cd0a30d219c99dbee6
Cedric Levasseur <cedric.levasseur@gmail.com>
2011-01-24 09:10:39 +0100

    Changing the load method, and the file format

---
+++ src/main/groovy/FileToCheck.groovy
@@ -0,0 +1,33 @@
+public class FileToCheck{
+
+	private String url;
+	private double size;
+	private double delta;
+
+	public void setUrl(String _url){
+		url=_url
+	}
+	
+	public String getUrl(){
+		return url
+	}
+
+	public void setSize(double _size){
+		url=url
+	}
+	
+	public double getSize(){
+		return size
+	}
+
+	public void setDelta(double _delta){
+		delta=_delta
+	}
+	
+	public double getDelta(){
+		return delta
+	}
+
+
+
+}
